0fcdebfbf7947d5343bcfce60c80912082c7e5db
pgmrDohan <dohan.k764@gmail.com>
2026-08-17 13:12:09 +0900

    feat: update layout styles and remove StatusBar component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,13 @@
 import {Outlet} from "react-router-dom";
 
 import {BottomNav} from "@/components/BottomNav";
-import {StatusBar} from "@/components/StatusBar";
 
 import styles from "./index.module.scss";
 
 export const Layout = () => {
   return (
     <div className={styles.app}>
-      <StatusBar />
-      <main className={styles.content}>
-        <Outlet />
-      </main>
+      <Outlet />
       <BottomNav />
     </div>
   );
